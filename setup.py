d8e674f4494462268fa696c995f33516d04a5fab
KelizaMcGuire <kelizamcguire@gmail.com>
2026-07-10 11:10:03 -0400

    Revert "make county question dependent on court division type"

--- setup.py
+++ setup.py
@@ -1,59 +1,18 @@
-import os
-import sys
-from setuptools import setup, find_namespace_packages
-from fnmatch import fnmatchcase
-from distutils.util import convert_path
+from setuptools import find_packages, setup
 
-standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
-standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
 
-def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
-    out = {}
-    stack = [(convert_path(where), '', package)]
-    while stack:
-        where, prefix, package = stack.pop(0)
-        for name in os.listdir(where):
-            fn = os.path.join(where, name)
-            if os.path.isdir(fn):
-                bad_name = False
-                for pattern in exclude_directories:
-                    if (fnmatchcase(name, pattern)
-                        or fn.lower() == pattern.lower()):
-                        bad_name = True
-                        break
-                if bad_name:
-                    continue
-                if os.path.isfile(os.path.join(fn, '__init__.py')):
-                    if not package:
-                        new_package = name
-                    else:
-                        new_package = package + '.' + name
-                        stack.append((fn, '', new_package))
-                else:
-                    stack.append((fn, prefix + name + '/', package))
-            else:
-                bad_name = False
-                for pattern in exclude:
-                    if (fnmatchcase(name, pattern)
-                        or fn.lower() == pattern.lower()):
-                        bad_name = True
-                        break
-                if bad_name:
-                    continue
-                out.setdefault(package, []).append(prefix+name)
-    return out
-
-setup(name='docassemble.VTCertificateOfServiceGeneral',
-      version='0.0.1',
-      description=('General Certificate of Service form 600-00264.'),
-      long_description='# docassemble.VTCertificateOfServiceGeneral\r\nGeneral Certificate of Service form 600-00264.\r\n\r\n## Authors\r\n- K. Surette, Legal Services Vermont\r\n- K. McGuire, Lemma Legal\r\n',
-      long_description_content_type='text/markdown',
-      author='Legal Services Vermont and Lemma Legal',
-      author_email='kelizamcguire@gmail.com',
-      license='',
-      url='https://docassemble.org',
-      packages=find_namespace_packages(),
-      install_requires=[],
-      zip_safe=False,
-      package_data=find_package_data(where='docassemble/VTCertificateOfServiceGeneral/', package='docassemble.VTCertificateOfServiceGeneral'),
-     )
+setup(
+    name="docassemble.VTCertificateOfServiceGeneral",
+    version="0.0.1",
+    description="General Certificate of Service form 600-00264.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Legal Services Vermont and Lemma Legal",
+    packages=find_packages(),
+    namespace_packages=["docassemble"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "docassemble",
+    ],
+)
